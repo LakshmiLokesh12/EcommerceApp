fb183936e001d6d7c9d508ec6c4f65a0a027e73f
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-01-22 23:19:17 +0530

    controller findall unit test done

--- tests/unit/controllers/category.controller.js
+++ tests/unit/controllers/category.controller.js
@@ -26,8 +26,79 @@ describe("Testing create category method",()=>{
         expect(res.send).toHaveBeenCalledWith(newCategory);
     });
 
-    it("test failure during creation of a category",()=>{
-
+    it("test failure during creation of a category",async()=>{
+        const spy = jest.spyOn(CategoryModel, 'create')
+                     .mockImplementation(() => Promise.reject(
+                                                Error("This is an error."))
+                     );
+      
+      await CategoryController.create(req, res);               
+   
+      await expect(spy).toHaveBeenCalled();
+      expect(CategoryModel.create).toHaveBeenCalledWith(newCategory);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.send).toHaveBeenCalledWith({
+         message: "Some Internal error while storing the category!"
     });
+});
 
 })
+
+describe('CategoryController.findAll', () => {
+
+    it('should call CategoryController.findAll method with empty query value', async () => {
+       const spy = jest.spyOn(CategoryModel, 'findAll')
+                      .mockImplementation(
+                         () => Promise.resolve(newCategory)
+                      );
+ 
+       req.query= {
+          name : ''
+       };
+       await CategoryController.findAll(req, res);
+ 
+       expect(spy).toHaveBeenCalled();
+       expect(CategoryModel.findAll).toHaveBeenCalled();
+       expect(res.status).toHaveBeenCalledWith(200);
+       expect(res.send).toHaveBeenCalledWith(newCategory);
+    });
+ 
+    it('should call CategoryController.findAll method with a query value', async () => {
+       const queryParam = {
+          where: {
+             name: 'Electronics'
+          }
+       };
+       const spy = jest.spyOn(CategoryModel, 'findAll')
+                      .mockImplementation(
+                         (queryParam) => Promise.resolve(newCategory)
+                      );
+                      
+       req.query= {
+          name : 'Electronics'
+       }
+       await CategoryController.findAll(req, res);
+ 
+       expect(spy).toHaveBeenCalled();
+       expect(CategoryModel.findAll).toHaveBeenCalledWith(queryParam);
+       expect(res.status).toHaveBeenCalledWith(200);
+       expect(res.send).toHaveBeenCalledWith(newCategory);
+    });
+ 
+    it('should call CategoryController.findAll method and ends with a error', async () => {
+       const spy = jest.spyOn(CategoryModel, 'findAll')
+                      .mockImplementation(() => Promise.reject(
+                                                 Error("This is an error."))
+                      );
+       
+       await CategoryController.findAll(req, res);               
+    
+       await expect(spy).toHaveBeenCalled();
+       expect(CategoryModel.findAll).toHaveBeenCalled();
+       expect(res.status).toHaveBeenCalledWith(500);
+       expect(res.send).toHaveBeenCalledWith({
+          message: "Some Internal error while fetching all the categories"
+       });
+    });
+ });
+ 
