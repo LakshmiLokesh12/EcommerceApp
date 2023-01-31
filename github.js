73a294a72cbdb6d451605db13bceca6bbf23acc9
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-01-31 23:11:54 +0530

    done

---
+++ github.js
@@ -0,0 +1,15 @@
+/**
+ * steps to raise pr
+ * 
+ * https://github.com/Vishwa07dev/DemoGitHubRepo
+ * 
+ * 1.fork it 
+ * clone it- git clone url
+ * create branch "students"
+ * -git checkout -b students
+ * create file inside folder abc.txt
+ * git add .
+ * git commit -m "commiting new file"
+ * git push -u origin student
+ * 
+ *///go to git hub url raise PR
