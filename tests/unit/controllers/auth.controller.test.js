ee9ca888af9509c945a1ffff44d4faa1af8f0d5e
LakshmiLokesh12 <100308472+LakshmiLokesh12@users.noreply.github.com>
2023-01-31 21:46:21 +0530

    changes done

--- tests/unit/controllers/auth.controller.test.js
+++ tests/unit/controllers/auth.controller.test.js
@@ -1,84 +1,87 @@
 //const jwt = require("jsonwebtoken");
-const config = require('../../../configs/secret.config');
-const authController = require('../../../controllers/auth.controller');
-const Models = require('../../../models');
+const config = require("../../../configs/secret.config");
+const authController = require("../../../controllers/auth.controller");
+const Models = require("../../../models");
 const UserModel = Models.user;
 const RoleModel = Models.role;
-const newUser = require('../../mockData/newUser.json');
-const userData = require('../../mockData/userData.json');
-const { mockRequest, mockResponse } = require("./interceptor");
+const newUser = require("../../mockData/newUser.json");
+const userData = require("../../mockData/userData.json");
+const { mockRequest, mockResponse } = require("../interceptor");
 const bcrypt = require("bcryptjs");
 //
 //
 
 let req, res;
 
-beforeEach(() =>{
-    req = mockRequest();
-    res = mockResponse();
-})
+beforeEach(() => {
+  req = mockRequest();
+  res = mockResponse();
+});
 
-describe('authController.signup', () => {
+describe("authController.signup", () => {
+  beforeEach(() => {
+    req.body = newUser;
+  });
 
-    beforeEach(() => {
-        req.body = newUser;
+  const resFromCreate = {
+    setRoles: async () => Promise.resolve(),
+  };
+
+  it("should return user registered success message", async () => {
+    const spyOnCreate = jest
+      .spyOn(UserModel, "create")
+      .mockImplementation(() => Promise.resolve(resFromCreate));
+    const spyOnFindAll = jest
+      .spyOn(RoleModel, "findAll")
+      .mockImplementation(() => Promise.resolve());
+
+    await authController.signup(req, res);
+
+    await expect(spyOnCreate).toHaveBeenCalled();
+    await expect(spyOnFindAll).toHaveBeenCalled();
+    await expect(UserModel.create).toHaveBeenCalled();
+    await expect(RoleModel.findAll).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "User registered successfully!",
     });
+  });
 
-    const resFromCreate = {
-        setRoles: async () => Promise.resolve(),
-    }
+  it("should return user registered success message when no roles is passed", async () => {
+    const spyOnCreate = jest
+      .spyOn(UserModel, "create")
+      .mockImplementation(() => Promise.resolve(resFromCreate));
+    const spyOnFindAll = jest
+      .spyOn(RoleModel, "findAll")
+      .mockImplementation(() => Promise.resolve());
+    req.body.roles = null;
 
-    it('should return user registered success message', async () => {
-        const spyOnCreate = jest.spyOn(UserModel, 'create')
-                                .mockImplementation(() => Promise.resolve(resFromCreate)
-                                );
-        const spyOnFindAll = jest.spyOn(RoleModel, 'findAll')
-                                .mockImplementation(() => Promise.resolve()
-                                );
+    await AuthController.signup(req, res);
 
-        await authController.signup(req, res);
-        
-        await expect(spyOnCreate).toHaveBeenCalled();
-        await expect(spyOnFindAll).toHaveBeenCalled();
-        await expect(UserModel.create).toHaveBeenCalled();
-        await expect(RoleModel.findAll).toHaveBeenCalled();
-        expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.send).toHaveBeenCalledWith({ message: "User registered successfully!" });    
+    await expect(spyOnCreate).toHaveBeenCalled();
+    //await expect(spyOnFindAll).not.toHaveBeenCalled();
+    await expect(UserModel.create).toHaveBeenCalled();
+    await expect(RoleModel.findAll).toHaveBeenCalledTimes(1); //in last test case
+    await expect(res.send).toHaveBeenCalledWith({
+      message: "User  successfully registered",
     });
+  });
 
-    it('should return user registered success message when no roles is passed', async () => {
-        const spyOnCreate = jest.spyOn(UserModel, 'create')
-                                .mockImplementation(() => Promise.resolve(resFromCreate)
-                                );
-        const spyOnFindAll = jest.spyOn(RoleModel, 'findAll')
-                                .mockImplementation(() => Promise.resolve()
-                                );
-        req.body.roles = null;
+  it("should return error message", async () => {
+    const spyOnCreate = jest
+      .spyOn(UserModel, "create")
+      .mockImplementation(() => Promise.reject(Error("This is an Error")));
+    const spyOnFindAll = jest
+      .spyOn(RoleModel, "findAll")
+      .mockImplementation(() => Promise.resolve());
+    req.body.roles = null;
 
-        await AuthController.signup(req, res);
-        
-        await expect(spyOnCreate).toHaveBeenCalled();
-        //await expect(spyOnFindAll).not.toHaveBeenCalled();
-        await expect(UserModel.create).toHaveBeenCalled();
-        await expect(RoleModel.findAll).toHaveBeenCalledTimes(1);//in last test case
-        await expect(res.send).toHaveBeenCalledWith({ message: "User  successfully registered" });    
-    });
+    await AuthController.signup(req, res);
 
-    it('should return error message', async () => {
-        const spyOnCreate = jest.spyOn(UserModel, 'create')
-                                .mockImplementation(() => Promise.reject(Error('This is an Error'))
-                                );
-        const spyOnFindAll = jest.spyOn(RoleModel, 'findAll')
-                                .mockImplementation(() => Promise.resolve()
-                                );
-        req.body.roles = null;
-
-        await AuthController.signup(req, res);
-        
-        await expect(spyOnCreate).toHaveBeenCalled();
-        //await expect(spyOnFindAll).not.toHaveBeenCalled();
-        await expect(UserModel.create).toHaveBeenCalled();
-        //await expect(RoleModel.findAll).not.toHaveBeenCalled();
-        expect(res.send).toHaveBeenCalledWith({ message: "This is an Error" });    
-    });
+    await expect(spyOnCreate).toHaveBeenCalled();
+    //await expect(spyOnFindAll).not.toHaveBeenCalled();
+    await expect(UserModel.create).toHaveBeenCalled();
+    //await expect(RoleModel.findAll).not.toHaveBeenCalled();
+    expect(res.send).toHaveBeenCalledWith({ message: "This is an Error" });
+  });
 });
--- tests/unit/controllers/category.controller.js
+++ tests/unit/controllers/category.controller.js
@@ -1,104 +1,94 @@
-const {mockRequest, mockResponse} = require("./interceptor");
+const { mockRequest, mockResponse } = require("../interceptor");
 const newCategory = require("../mockData/newCategory.json");
 const Category = require("../../../models").category;
 const categoryController = require("../../../controllers/category.controller");
 
-
-
-
-beforeEach(()=>{
-    req = mockRequest();
-    res = mockResponse();
-})
-
-
-
-describe("Testing create category method",()=>{
-    it('test successfull creation of a new Category', async() =>{
-        req.body = newCategory
-        const spy = jest.spyOn(Category,'create').mockImplementation((newCategory)=>Promise.resolve(newCategory))
-        
-        await categoryController.create(req,res);
-
-        expect(spy).toHaveBeenCalled();
-        expect(Category.create).toHaveBeenCalledWith(newCategory);
-        expect(res.status).toHaveBeenCalledWith(201);
-        expect(res.send).toHaveBeenCalledWith(newCategory);
-    });
-
-    it("test failure during creation of a category",async()=>{
-        const spy = jest.spyOn(CategoryModel, 'create')
-                     .mockImplementation(() => Promise.reject(
-                                                Error("This is an error."))
-                     );
-      
-      await CategoryController.create(req, res);               
-   
-      await expect(spy).toHaveBeenCalled();
-      expect(CategoryModel.create).toHaveBeenCalledWith(newCategory);
-      expect(res.status).toHaveBeenCalledWith(500);
-      expect(res.send).toHaveBeenCalledWith({
-         message: "Some Internal error while storing the category!"
-    });
+beforeEach(() => {
+  req = mockRequest();
+  res = mockResponse();
 });
 
-})
+describe("Testing create category method", () => {
+  it("test successfull creation of a new Category", async () => {
+    req.body = newCategory;
+    const spy = jest
+      .spyOn(Category, "create")
+      .mockImplementation((newCategory) => Promise.resolve(newCategory));
 
-describe('CategoryController.findAll', () => {
+    await categoryController.create(req, res);
 
-    it('should call CategoryController.findAll method with empty query value', async () => {
-       const spy = jest.spyOn(CategoryModel, 'findAll')
-                      .mockImplementation(
-                         () => Promise.resolve(newCategory)
-                      );
- 
-       req.query= {
-          name : ''
-       };
-       await CategoryController.findAll(req, res);
- 
-       expect(spy).toHaveBeenCalled();
-       expect(CategoryModel.findAll).toHaveBeenCalled();
-       expect(res.status).toHaveBeenCalledWith(200);
-       expect(res.send).toHaveBeenCalledWith(newCategory);
+    expect(spy).toHaveBeenCalled();
+    expect(Category.create).toHaveBeenCalledWith(newCategory);
+    expect(res.status).toHaveBeenCalledWith(201);
+    expect(res.send).toHaveBeenCalledWith(newCategory);
+  });
+
+  it("test failure during creation of a category", async () => {
+    const spy = jest
+      .spyOn(CategoryModel, "create")
+      .mockImplementation(() => Promise.reject(Error("This is an error.")));
+
+    await CategoryController.create(req, res);
+
+    await expect(spy).toHaveBeenCalled();
+    expect(CategoryModel.create).toHaveBeenCalledWith(newCategory);
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Some Internal error while storing the category!",
     });
- 
-    it('should call CategoryController.findAll method with a query value', async () => {
-       const queryParam = {
-          where: {
-             name: 'Electronics'
-          }
-       };
-       const spy = jest.spyOn(CategoryModel, 'findAll')
-                      .mockImplementation(
-                         (queryParam) => Promise.resolve(newCategory)
-                      );
-                      
-       req.query= {
-          name : 'Electronics'
-       }
-       await CategoryController.findAll(req, res);
- 
-       expect(spy).toHaveBeenCalled();
-       expect(CategoryModel.findAll).toHaveBeenCalledWith(queryParam);
-       expect(res.status).toHaveBeenCalledWith(200);
-       expect(res.send).toHaveBeenCalledWith(newCategory);
+  });
+});
+
+describe("CategoryController.findAll", () => {
+  it("should call CategoryController.findAll method with empty query value", async () => {
+    const spy = jest
+      .spyOn(CategoryModel, "findAll")
+      .mockImplementation(() => Promise.resolve(newCategory));
+
+    req.query = {
+      name: "",
+    };
+    await CategoryController.findAll(req, res);
+
+    expect(spy).toHaveBeenCalled();
+    expect(CategoryModel.findAll).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith(newCategory);
+  });
+
+  it("should call CategoryController.findAll method with a query value", async () => {
+    const queryParam = {
+      where: {
+        name: "Electronics",
+      },
+    };
+    const spy = jest
+      .spyOn(CategoryModel, "findAll")
+      .mockImplementation((queryParam) => Promise.resolve(newCategory));
+
+    req.query = {
+      name: "Electronics",
+    };
+    await CategoryController.findAll(req, res);
+
+    expect(spy).toHaveBeenCalled();
+    expect(CategoryModel.findAll).toHaveBeenCalledWith(queryParam);
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith(newCategory);
+  });
+
+  it("should call CategoryController.findAll method and ends with a error", async () => {
+    const spy = jest
+      .spyOn(CategoryModel, "findAll")
+      .mockImplementation(() => Promise.reject(Error("This is an error.")));
+
+    await CategoryController.findAll(req, res);
+
+    await expect(spy).toHaveBeenCalled();
+    expect(CategoryModel.findAll).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(500);
+    expect(res.send).toHaveBeenCalledWith({
+      message: "Some Internal error while fetching all the categories",
     });
- 
-    it('should call CategoryController.findAll method and ends with a error', async () => {
-       const spy = jest.spyOn(CategoryModel, 'findAll')
-                      .mockImplementation(() => Promise.reject(
-                                                 Error("This is an error."))
-                      );
-       
-       await CategoryController.findAll(req, res);               
-    
-       await expect(spy).toHaveBeenCalled();
-       expect(CategoryModel.findAll).toHaveBeenCalled();
-       expect(res.status).toHaveBeenCalledWith(500);
-       expect(res.send).toHaveBeenCalledWith({
-          message: "Some Internal error while fetching all the categories"
-       });
-    });
- });
- 
+  });
+});
